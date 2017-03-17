bc148667610c096f2e36ce312fa4a3d8e639f2fd
Ben Gamari <ben@smart-cactus.org>
2017-03-17 11:02:16 -0400

    Print return value of hello

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int hello(void);
 int main() {
         int *prefix_data = (int*) &hello;
         printf("hi: %d\n", prefix_data[-1]);
-        hello();
+        printf("hi: %d\n", hello());
         return 0;
 }
